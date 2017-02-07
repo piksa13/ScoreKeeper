718e84e71a5fc68c79f851ffca5ac0a1d3a576c8
piksa13 <ania.piktas@gmail.com>
2017-02-07 16:08:52 +0000

    Score 4 fixed, missing Re.id in displayForPLayer1 added.

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
 
     int score, score2, score3, score4;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
